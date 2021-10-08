e922359fc73e7fc5a1d79de0e69e36d83dd05490
Saumya <76907411+saumya-703@users.noreply.github.com>
2021-10-08 08:18:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
     };
 
   }); 
+  window.onscroll = function() {
+      myFunction()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
